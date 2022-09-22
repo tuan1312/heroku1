f9f164d40ded1425a2484d64f5d961202f3de88a
tuan1312 <113591813+tuan1312@users.noreply.github.com>
2022-09-22 17:09:05 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require("http")
+const port = 3000;
+
+const app = http.createServer((req, res) => {
+    res.write("<h1>hrllasdisiski</h1")
+    res.write("<h3>15156565555</h3>")
+    res.end();
+})
+app.listen(port)
